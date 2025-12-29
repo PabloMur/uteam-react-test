2a35f11be1374c89af302d533e35070b5f9fb55d
PabloMur <polillomurillo@gmail.com>
2025-12-29 19:53:51 -0300

    second, realizando un mvp que cumpla con los requisitos de la prueba

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,5 @@
+const Navbar = () =>{
+    return <nav>Esta es la barra de navegacion</nav>
+}
+
+export {Navbar}
---
+++ src/services/ApiCalls/index.js
@@ -0,0 +1,4 @@
+/* En este documento lo que voy a hacer es exportar todas 
+   las funciones que van a usar los componentes para interactuar 
+   con la API
+*/
